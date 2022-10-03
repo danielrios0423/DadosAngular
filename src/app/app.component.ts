880f1a292ebf74f0024eff50648af7c5c60f6c46
Daniel Rios <danifelrios.dr@gmail.com>
2022-10-03 17:23:30 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Dados';
+  dadoLeft = '../assets/img/dice1.png'
+  dadoRight = '../assets/img/dice1.png'
+
+  num1 = 0;
+  num2 = 0;
+
+  tirarDados(): void{
+    this.num2 = Math.floor(Math.random() * 6) + 1;
+    this.num1 = Math.floor(Math.random() * 6) + 1;
+
+    this.dadoLeft = '../assets/img/dice' + this.num1 + '.png'
+    this.dadoRight = '../assets/img/dice' + this.num2 + '.png'
+    console.log(this.num1, this.num2)
+  }
+
 }
